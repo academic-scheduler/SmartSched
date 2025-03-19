492e6e4da41034400e5cb11095995380a01de224
Hasintha Madushan <hasinthagunawardana2000@gmail.com>
2025-03-19 20:37:10 +0530

    feat:update files

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -3,6 +3,7 @@ import api from "../api";
 import { useNavigate } from "react-router-dom";
 import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants";
 import "../styles/Form.css"
+import "../components/LoadingIndicator"
 
 function Form({ route, method }) {
      const [username, setUsername] = useState("");
