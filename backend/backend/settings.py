5df4bd667d3e76c64adbe0dbfd06b6ef5b682958
Hasintha Madushan <hasinthagunawardana2000@gmail.com>
2025-03-19 14:26:15 +0530

    feat:create serializer.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
-from dotenv import load_dotenv # type: ignore
+from dotenv import load_dotenv
 import os
 
 load_dotenv()
